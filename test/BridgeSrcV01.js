0b7e68d5dc8def9b7147472a80bc959a6586c7f5
nenofi <nenofi@proton.me>
2022-07-05 20:53:45 +0700

    BridgeSrcV01 test

--- test/BridgeSrcV01.js
+++ test/BridgeSrcV01.js
@@ -18,10 +18,22 @@ describe("NenoBridgeSrcV01", function () {
   it("Deploy Mock Tokens (IDRT and BIDR)", async function () {
     [owner, user1, user2, user3, bridge, exploiter] = await ethers.getSigners(4);
 
-    const BridgeSrc = await ethers.getContractFactory("NenoBridgeSrcV01");
-    bridgeSrc = await BridgeSrc.deploy('0x273a4fFcEb31B8473D51051Ad2a2EdbB7Ac8Ce02', 4, 4002, false);
+    const token = await ethers.getContractFactory("MockToken");
+    bidr = await token.deploy('BIDR', 'BIDR', 18);
+    idrt = await token.deploy('IDRT', 'IDRT', 2);
 
-    await bridgeSrc.deployed();
+    // 1 million bidr
+    await bidr.mint(user1.address, ethers.BigNumber.from("1000000000000000000000000"));
+    await bidr.mint(user2.address, ethers.BigNumber.from("1000000000000000000000000"));
+    await bidr.mint(user3.address, ethers.BigNumber.from("1000000000000000000000000"));
+
+    // 1 million idrt
+    await idrt.mint(user1.address, ethers.BigNumber.from("100000000"));
+    await idrt.mint(user2.address, ethers.BigNumber.from("100000000"));
+    await idrt.mint(user3.address, ethers.BigNumber.from("100000000"));
+
+    await bidr.deployed();
+    await idrt.deployed();
   });
 
   it("Deploy Bridge Source V01", async function () {
